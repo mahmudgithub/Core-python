e157a5db7405f744aca9579e84300e08d8264494
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-12 04:41:05 +0000

    use find method

--- python_programming_drive/Pactics Folder/BasicOne.py
+++ python_programming_drive/Pactics Folder/BasicOne.py
@@ -1,3 +1,15 @@
-//12.03.2021 pactics 
+# 12.03.2021 pactics 
 name='mahmud hossain'
 print(name)
+# arry in python
+arr=['mahmud','hossain','nannu']
+print(arr[1])
+# add item in arry
+arr.append('lamyaa')
+print(arr)
+# add to arry
+arr2=['bangladersj','pakistan','sodia']
+print(arr+arr2)
+# find method to find any specific item and its location from string
+country='Once upon a time our countruy bangladesh have agood resource of fish ,but recently bangladesh is not enough its'
+print(country.find('bangladesh'))
