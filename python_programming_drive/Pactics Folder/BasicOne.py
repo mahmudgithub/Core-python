28f8f4714de689397331364a64f396b771b33ed2
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-12 06:15:29 +0000

    python dicsonary

--- python_programming_drive/Pactics Folder/BasicOne.py
+++ python_programming_drive/Pactics Folder/BasicOne.py
@@ -1,30 +1,38 @@
-# 12.03.2021 pactics 
-name='mahmud hossain'
-print(name)
-# arry in python
-arr=['mahmud','hossain','nannu']
-print(arr[1])
-# add item in arry
-arr.append('lamyaa')
-print(arr)
-# add to arry
-arr2=['bangladersj','pakistan','sodia']
-print(arr+arr2)
-# find method to find any specific item and its location from string
-country='Once upon a time our countruy bangladesh have agood resource of fish ,but recently bangladesh is not enough its'
-print(country.find('bangladesh'))
-# use replace method to replace any item in predefined string value
-one=country.replace('bangladesh','islamic')
-print(one)
-# upper case a string
-two='bangladesh'
-print(two.upper())
-# lower case a string
-three='BANGLADESH'
-print(three.lower())
-# split method separate eatch space and show an array list
-print(country.split())
-# count method count repeted string in a full string
-print (country.count('bangladesh'))
-# startwith method check specific starting string and answer true and false
-print(country.startswith('Once'))
+# # 12.03.2021 pactics 
+# name='mahmud hossain'
+# print(name)
+# # arry in python
+# arr=['mahmud','hossain','nannu']
+# print(arr[1])
+# # add item in arry
+# arr.append('lamyaa')
+# print(arr)
+# # add to arry
+# arr2=['bangladersj','pakistan','sodia']
+# print(arr+arr2)
+# # find method to find any specific item and its location from string
+# country='Once upon a time our countruy bangladesh have agood resource of fish ,but recently bangladesh is not enough'
+# print(country.find('bangladesh'))
+# # use replace method to replace any item in predefined string value
+# one=country.replace('bangladesh','islamic')
+# print(one)
+# # upper case a string
+# two='bangladesh'
+# print(two.upper())
+# # lower case a string
+# three='BANGLADESH'
+# print(three.lower())
+# # split method separate eatch space and show an array list
+# print(country.split())
+# # count method count repeted string in a full string
+# print (country.count('bangladesh'))
+# # startwith method check specific starting string and answer true and false
+# print(country.startswith('Once'))
+# # endwith method ,check specific string at end
+# name='mahmud'
+# print(name.endwith(d))
+
+# python dicsonry
+names={'first':'mahmud','second':'hossain'}
+print(names)
+print(names['first'])
