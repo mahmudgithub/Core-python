2f3b107f511aa2b961f884de5de2e9f4716a13eb
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-05 13:00:08 +0100

    use for loop in array

--- pactics folder/array datastructure.py
+++ pactics folder/array datastructure.py
@@ -1,4 +1,11 @@
-arr=['mahmud','hossain','nannu']
-for x in arr:
-    print(x)
+# arr=['mahmud','hossain','nannu']
+# for x in arr:
+#     print(x)
 
+# # make  a empty array datastructure 
+# arr=[]
+# arr=input('inter an number:')
+# print(arr)
+
+# for x in arr:
+#     print(x)
