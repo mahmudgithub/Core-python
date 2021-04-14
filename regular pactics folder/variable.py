4b2d6b65522c74d303d7870d7790d61dfb7f5968
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-14 14:24:24 +0100

    python type casting

--- regular pactics folder/variable.py
+++ regular pactics folder/variable.py
@@ -1,9 +1,19 @@
-# different type of variables 
-# intiger type
-num=1345
+# # different type of variables 
+# # intiger type
+# num=1345
+# print(num)
+# print(type(num))
+# # strinf type variable
+# name='mahmud'
+# print(name)
+# print(type(name))
+
+
+
+# variable type conversion /type casting 
+num=3
 print(num)
 print(type(num))
-# strinf type variable
-name='mahmud'
-print(name)
-print(type(name))
+num2=str(num)
+print(num2)
+print(type(num2))
