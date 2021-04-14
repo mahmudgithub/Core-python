616d918e3ea3fd822aee955662af79da51f894f5
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-14 17:24:33 +0100

    clear method

--- arraybasic.py
+++ arraybasic.py
@@ -1,15 +1,62 @@
 # make an array
 names=['mahud','hossain','nannu']
-# access array element
-print(names)
-# access by index number
-print(names[1])
-# change array values by index number
-names[2]='lamyaa'
-print(names)
-# find length of an array 
-# same function in javascript use length 
-print(len(names))
-# # add new value to array 
-# names.push[3]='new'
+
+
+
+# # access array element
 # print(names)
+
+
+
+# # access by index number
+# print(names[1])
+
+
+
+# # change array values by index number
+# names[2]='lamyaa'
+# print(names)
+
+
+# #appent method
+# append add new array elemet in last 
+# # add new value to array by append method
+# # same work in javascript use by only index number or push method 
+# names.append('new')
+# print(names)
+
+# #pop method
+# # remove any array index elmen 
+# # remove array element by pop
+# # same work in javascript by pop method 
+# names.pop(1)
+# print(names)
+
+
+# # remove method 
+# # using this method remove any array elment by element name
+# names.remove('nannu')
+# print(names)
+
+
+
+# # length method 
+# # find length of an array 
+# # same function in javascript use length 
+# print(len(names))
+
+
+
+# # for loop in array 
+# # same work in javascript by same as c format
+# for x in names:
+#     print(x)
+
+
+# clear metthod 
+# remove all element from array 
+names.clear()
+print(names)
+
+
+
