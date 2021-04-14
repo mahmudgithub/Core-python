f530837942bf85b9d15510f2588c0a9deb7e3f1a
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-14 16:34:01 +0100

    work with array basic

---
+++ arraybasic.py
@@ -0,0 +1,15 @@
+# make an array
+names=['mahud','hossain','nannu']
+# access array element
+print(names)
+# access by index number
+print(names[1])
+# change array values by index number
+names[2]='lamyaa'
+print(names)
+# find length of an array 
+# same function in javascript use length 
+print(len(names))
+# # add new value to array 
+# names.push[3]='new'
+# print(names)
