b606f5198e7d8d7230cebf6da2e0522448685ea3
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-14 14:38:05 +0100

    variable name decliaration style

--- regular pactics folder/variable.py
+++ regular pactics folder/variable.py
@@ -10,13 +10,38 @@
 
 
 
-# variable type conversion /type casting 
-num=3
-print(num)
-print(type(num))
-num2=str(num)
-print(num2)
-print(type(num2))
-num3=float(num)
-print(num3)
-print(type(num3))
+# # variable type conversion /type casting 
+# num=3
+# print(num)
+# print(type(num))
+# num2=str(num)
+# print(num2)
+# print(type(num2))
+# num3=float(num)
+# print(num3)
+# print(type(num3))
+
+
+
+
+# variable names
+# camel case style
+myNymeIs='mahmud'
+print(myNymeIs)
+# pascal style
+MyNameIs='hossain'
+print(MyNameIs)
+# Snake Case
+_my_name_is='lamyaa'
+print(_my_name_is)
+# more accepted styles
+myname='mahmud'
+print(myname)
+my_='hossain'
+print(my_)
+myname5='mahmud'
+print(myname5)
+# invalide styles
+# 2myname='hossain' donot use number at start
+# my-name='madhdh' do not use minus 
+# my name='dhdhdh'  do not use space btween 
