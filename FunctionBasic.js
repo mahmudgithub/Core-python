741b9e0a8a48f36ff9132c07b04bd126be8003e4
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-17 15:02:16 +0100

    parametize arguments

--- FunctionBasic.js
+++ FunctionBasic.js
@@ -4,13 +4,22 @@
 // }
 // fn()
 
+
+
+
 // // argument function 
 // function fun1(x,y){
 //     console.log();
 // }
 
-// in javascript call rast parameter or python call arbitary parameter
-  function fun3(...args){
-      console.log(args);
-  }
-  fun3(1,2,3,4,5)
+
+
+// // in javascript call rast parameter or python call arbitary parameter
+//   function fun2(...args){
+//       console.log(args);
+//   }
+//   fun2(1,2,3,4,5)
+
+
+// # keyword argument function
+// not possible 
--- regular pactics folder/FunctionBasic.py
+++ regular pactics folder/FunctionBasic.py
@@ -3,12 +3,31 @@
 #     print('i am first function')
 # fun()
 
+
+
+
 # # argument functoion 
 # def fun1(x,y):
 #     print((x+y))
 # fun1(2,2)
 
-# arbitary argument function ,in javasctript its call rest parameter
-def fun2(*args):
-    print(args)
-fun2(1,2,3,4,5)
+
+
+
+# # arbitary argument function ,in javasctript its call rest parameter
+# def fun2(*args):
+#     print(args)
+# fun2(1,2,3,4,5)
+
+
+
+# # keyword argument function
+# def fun3(x,y,z):
+#     print(x)
+#     print(y)
+#     print(z)
+# fun3(x='mahmud',y='hossain',z='nannu')
+
+# def fun4(**kwargs):
+#     print(kwargs)
+# fun4(a='mahmud',b='hossain')
