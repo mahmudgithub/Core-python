682881727614509889e82093f00ff4a13d51334e
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-17 14:24:41 +0100

    argumet function

--- FunctionBasic.js
+++ FunctionBasic.js
@@ -1,4 +1,10 @@
+// basic function declaration 
 function fn(){
     console.log('helo');
 }
 fn()
+
+// argument function 
+function fun1(x,y){
+    console.log();
+}
--- regular pactics folder/FunctionBasic.py
+++ regular pactics folder/FunctionBasic.py
@@ -1,3 +1,9 @@
+# basic function declaration 
 def fun():
     print('i am first function')
 fun()
+
+# argument functoion 
+def fun1(x,y):
+    print((x+y))
+fun1(2,2)
