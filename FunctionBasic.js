1a9be23b378cc04bf538d251ec6713689e2e9f0a
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-17 14:30:43 +0100

    arbitary parameter function or rest parameter function

--- FunctionBasic.js
+++ FunctionBasic.js
@@ -1,10 +1,16 @@
-// basic function declaration 
-function fn(){
-    console.log('helo');
-}
-fn()
+// // basic function declaration 
+// function fn(){
+//     console.log('helo');
+// }
+// fn()
 
-// argument function 
-function fun1(x,y){
-    console.log();
-}
+// // argument function 
+// function fun1(x,y){
+//     console.log();
+// }
+
+// in javascript call rast parameter or python call arbitary parameter
+  function fun3(...args){
+      console.log(args);
+  }
+  fun3(1,2,3,4,5)
--- regular pactics folder/FunctionBasic.py
+++ regular pactics folder/FunctionBasic.py
@@ -1,9 +1,14 @@
-# basic function declaration 
-def fun():
-    print('i am first function')
-fun()
+# # basic function declaration 
+# def fun():
+#     print('i am first function')
+# fun()
 
-# argument functoion 
-def fun1(x,y):
-    print((x+y))
-fun1(2,2)
+# # argument functoion 
+# def fun1(x,y):
+#     print((x+y))
+# fun1(2,2)
+
+# arbitary argument function ,in javasctript its call rest parameter
+def fun2(*args):
+    print(args)
+fun2(1,2,3,4,5)
